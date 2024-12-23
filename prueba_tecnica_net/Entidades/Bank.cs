fae007575ec8917df34f7936f46fcccd1973c761
Javi <jcazorla500@gmail.com>
2024-12-23 19:22:59 +0100

    Creación de solution y gitignore. Agregada clase Bank

---
+++ prueba_tecnica_net/Entidades/Bank.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace prueba_tecnica_net.Entidades;
+
+public class Bank
+{
+    [MaxLength(75)]
+    string Name { get; set; } = default!;
+
+    [MaxLength(10)]
+    string Bic { get; set; } = default!;
+
+    [MaxLength(2)]
+    string Country { get; set; } = default!;
+}
